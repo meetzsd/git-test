3b50df2aa2d2789506b76a0524f51389351a8a4c
meetzsd <meetzsd@gmail.com>
2020-12-31 16:50:49 +0800

    test1

--- hello/src/main/java/org/example/App.java
+++ hello/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("22222");
+        System.out.println("33333");
     }
 }
